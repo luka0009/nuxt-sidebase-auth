9e80a299e655ec3e8d24689f475ead49b8351300
luka <lukabarbakadze18@gmail.com>
2023-09-04 14:34:12 +0400

    first commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>Dashboard</div>
+	<button @click="logOutUser">Sign out</button>
+</template>
+
+<script setup lang="ts">
+	definePageMeta({ middleware: "auth" });
+
+	const { signOut } = useAuth();
+
+	const logOutUser = async () => {
+		await signOut({ callbackUrl: "/" });
+	};
+</script>
+
+<style scoped></style>
